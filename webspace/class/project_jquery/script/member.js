4d4ff542d1692038937e3cb93b1b4f819f9b5c3d
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-07-13 12:58:08 +0900

    css

---
+++ webspace/class/project_jquery/script/member.js
@@ -0,0 +1,12 @@
+// 회원의 정보 : 아이디, 비밀번호, 이름
+// Member -> 생성자 함수를 정의
+
+function Member(id, pw, name) {
+    this.userid = id;
+    this.pw = pw;
+    this.username = name;
+}
+// 객체가 가지는 메소드는 공통으로 사용 -> prototype
+Member.prototype.makeHtml = function () {
+    return '[id:' + this.userid + ' , pw: ' + this.pw + ' , name: ' + this.username + ' ]'
+};
